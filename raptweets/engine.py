561302b1be39b72ba9094f5d0a3e4f62ccb89130
Aadil Bhatti <aadilzbhatti@gmail.com>
2015-10-24 17:42:36 -0500

    added alchemy api

---
+++ raptweets/engine.py
@@ -0,0 +1,2 @@
+from TwitterAPI import TwitterAPI
+from alchemyapi import AlchemyAPI
--- raptweets/models.py
+++ raptweets/models.py
@@ -4,3 +4,9 @@ class Tweet(models.Model):
     text = models.CharField(max_length=140)
     author = models.CharField(max_length=50)
     date = models.DateTimeField
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    release_date = models.DateTimeField
+    sales = models.IntegerField
