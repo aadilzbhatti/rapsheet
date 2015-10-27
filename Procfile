0da172240bc55bf243dd1be8040ee76d74d66437
Aadil Bhatti <aadilzbhatti@gmail.com>
2015-10-27 17:47:43 -0500

    deploy time

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rapsheet.wsgi --log-file -
--- rapsheet/settings.py
+++ rapsheet/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -101,3 +102,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DATABASES['default'] = dj_database_url.config()
