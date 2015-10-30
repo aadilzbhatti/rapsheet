41a5945254c2ba9d5ca44de420c53f13a9dc9670
Aadil Bhatti <aadilzbhatti@gmail.com>
2015-10-30 18:10:25 -0500

    lezdodis

--- rapsheet/wsgi.py
+++ rapsheet/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import django
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
@@ -15,5 +16,6 @@ from dj_static import Cling
 application = Cling(get_wsgi_application())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rapsheet.settings")
+django.setup()
 
 # application = get_wsgi_application()
